c594fdbd16dbdd32841985d684f0541a80bf6af3
ale <ruiz.ac@gmail.com>
2019-05-24 01:08:05 -0400

    Initial commit

---
+++ src/main/java/org/growind/App.java
@@ -0,0 +1,18 @@
+package org.growind;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        App app = new App();
+        app.process(args);
+        System.out.println( "Hello World!" );
+
+    }
+
+    public void process(String[] args) {
+        
+    }
+
+
+}
